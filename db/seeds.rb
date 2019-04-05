bcf51c1a73ae19417dc3820e60ecf05170d16a7f
cfinn16 <connorfinnegan16@gmail.com>
2019-04-05 11:07:07 -0400

    created schedule and appointment models, created db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Schedule.create(name: 'Schedule 1')
+Schedule.create(name: 'Schedule 2')
+Schedule.create(name: 'Schedule 3')
+
+Appointment.create(name: 'Meeting', start_time: 10, end_time: 15, schedule_id: 1)
+Appointment.create(name: 'Meeting', start_time: 20, end_time: 30, schedule_id: 1)
+Appointment.create(name: 'Meeting', start_time: 25, end_time: 32, schedule_id: 1)
